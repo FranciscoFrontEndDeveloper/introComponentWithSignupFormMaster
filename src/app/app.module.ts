d02be892d5b65e373f820c041dad725bda9f4898
FRANCISCO <pachonetworking@gmail.com>
2023-05-03 18:47:18 -0500

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TextComponent } from './components/text/text.component';
+import { FormComponent } from './components/form/form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
