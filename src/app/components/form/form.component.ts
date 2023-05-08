c850282d4c48b715c056b5b43aa0add0b1bfa1db
FRANCISCO <pachonetworking@gmail.com>
2023-05-07 23:19:33 -0500

    input to button

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -27,4 +27,8 @@ export class FormComponent {
       password: '',
     })
   }
+
+  onSubmitSignUp() {
+    console.log(this.formSignUp.value)
+  }
 }
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,24 +1,54 @@
 import { Component, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.sass'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: InputComponent,
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
-  @Input('InputType') InputType: string = '' ;
-  @Input('InputPlaceholder') InputPlaceholder: string = '' ;
+export class InputComponent implements ControlValueAccessor {
+  inputValueForm = '';
+
+  private onChangeFn!: Function;
+
+  changeText($event: any): void {
+    this.onChangeFn($event.target.value);
+  }
+
+  writeValue(value: string): void {
+    // throw new Error('Method not implemented.');
+    this.inputValueForm = value;
+  }
+  registerOnChange(fn: any): void {
+    // throw new Error('Method not implemented.');
+    this.onChangeFn = fn;
+  }
+  registerOnTouched(fn: any): void {
+    // throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    // throw new Error('Method not implemented.');
+  }
+  @Input('InputType') InputType: string = '';
+  @Input('InputPlaceholder') InputPlaceholder: string = '';
   @Input('InputValue') InputValue: string = '';
   styleInput: string = '';
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    // if (this.InputType === 'submit') {
-    //   this.styleInput = 'buttonStyle';
-    //   console.log('es un boton')
-    // } else {
-    //   this.styleInput = 'inputStyle';
-    //   console.log('es texto')
-    // }
+    if (this.InputType === 'submit') {
+      this.styleInput = 'buttonStyle';
+      console.log('es un boton')
+    } else {
+      this.styleInput = 'inputStyle';
+      console.log('es texto')
+    }
   }
 }
